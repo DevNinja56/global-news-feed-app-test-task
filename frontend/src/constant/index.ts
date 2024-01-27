3963529b0f4f97793cd28625b89f7ec1e5dd804b
DevNinja56 <danisamim9@gmail.com>
2024-01-28 01:08:50 +0500

    add constants

---
+++ frontend/src/constant/index.ts
@@ -0,0 +1,63 @@
+export const IS_FIRST_TIME = 'isFirstTime';
+export const LANGUAGE = 'language';
+export const COUNTRY = 'country';
+export const US = 'us';
+export const ENGLISH = 'en';
+export const SPLIT_FORMAT = '^^^^^';
+export const GOOGLE_DOMAIN = 'news.google.com';
+
+export const COUNTRY_OPTIONS = [
+  {
+    label: 'United States',
+    image: '/images/usaFlag.png',
+    value: 'us',
+  },
+  {
+    label: 'Pakistan',
+    image: '/images/pakFlag.png',
+    value: 'pk',
+  },
+  {
+    label: 'South Africa',
+    image: '/images/africa.png',
+    value: 'za',
+  },
+  {
+    label: 'United Kingdom',
+    image: '/images/ukFlag.png',
+    value: 'gb',
+  },
+  {
+    label: 'China',
+    image: '/images/chinaFlag.png',
+    value: 'zh',
+  },
+];
+
+export const LANGUAGE_OPTIONS = [
+  {
+    label: 'English',
+    image: '/images/ukFlag.png',
+    value: 'en',
+  },
+  {
+    label: 'Chinese',
+    image: '/images/chinaFlag.png',
+    value: 'zh',
+  },
+  {
+    label: 'Spanish',
+    image: '/images/spainFLag.png',
+    value: 'es',
+  },
+  {
+    label: 'Russian',
+    image: '/images/russiaFlag.png',
+    value: 'ru',
+  },
+  {
+    label: 'French',
+    image: '/images/franceFlag.png',
+    value: 'fr',
+  },
+];
